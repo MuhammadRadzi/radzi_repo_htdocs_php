e7c02db6c80fdfdb759e9ea6f14303003b05c784
MuhammadRadzi <nippon.pein14@gmail.com>
2025-04-28 08:42:30 +0800

    Quiz Logika If-Else Radzi

---
+++ tugas5-logika-radzi.php
@@ -0,0 +1,9 @@
+<?php
+$nilai_matematika = 75;
+$nilai_bahasa = 70;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >= 70) {
+    echo "Selamat! Kamu boleh nonton bioskop akhir pekan ini";
+} else {
+    echo "Yah... belajar lagi ya, lain kali bisa!";
+}
