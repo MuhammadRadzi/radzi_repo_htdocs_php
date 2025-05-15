bef7bdb792961e2e56138f0e79ffa71660ab1627
MuhammadRadzi <nippon.pein14@gmail.com>
2025-05-15 13:54:59 +0800

    Tugas Foreach Radzi

---
+++ tugas12-foreach-radzi.php
@@ -0,0 +1,13 @@
+<?php
+
+$sepatu = [
+    "Aerostreet",
+    "Compass",
+    "Ortus",
+    "Specs"
+];
+
+echo "Daftar sepatu: </br>";
+foreach ($sepatu as $merekSepatu) {
+    echo "- " .$merekSepatu ."</br>";
+}
